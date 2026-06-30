3c21751e149aa98d98f0f72a7d52f0810c2f169d
Tejas Avhad <codingwithtejas7@gmail.com>
2026-06-30 11:20:25 +0530

    refactor: clean up tsconfig.json and adjust trustedOrigins in auth configuration

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -24,4 +24,9 @@ export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
 
   baseURL: process.env.BETTER_AUTH_URL,
+
+  trustedOrigins: [
+  "http://localhost:3000",
+  process.env.TRUSTED_ORIGIN!,
+],
 });
